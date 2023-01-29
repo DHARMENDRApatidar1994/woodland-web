43458efd9adfea45f45f35fea88e6c11b5d27df3
dharmendra patidar <patidardharmendra1994@gmail.com>
2023-01-29 09:21:46 +0530

    commit first

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const menu = document.querySelector(".mobile-middle");
 const mobile = document.querySelector(".mob-middle1");
-
+flag = 0;
 menu.addEventListener("click", function(){
-    mobile.style.transform = "translateX(0)";
+    if (flag == 0){
+     mobile.style.transform = "translateX(0)";
+     flag = 1;
+    }
+    else{
+        mobile.style.transform = "translateX(-120%)";
+        flag = 0;
+    }
+    
 })
